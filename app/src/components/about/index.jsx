304dc0e03a202b8b16725febaf39db9c863b8ec1
Nikki Vargas <angela94.chavez@gmail.com>
2024-06-25 14:25:03 -0500

    changed css, added hover to project cards, changed background colors and font color, adjusted nav bar items to center.

--- app/src/components/about/index.jsx
+++ app/src/components/about/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './about.css'
+import React from 'react';
+import './about.css'; 
 
 const About = () => {
   return (
-    <div>About</div>
-    
-  )
+    <div>
+      <h1>About Me</h1>
+      <p>Welcome to my portfolio! Here you can learn more about me and my work.</p>
+    </div>
+  );
 }
 
-export default About
+export default About;
