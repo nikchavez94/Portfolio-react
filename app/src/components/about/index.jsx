308cca2de37471e7a1d75386fafe21a8c9c378ca
Nikki Vargas <nikkivargas@Nikkis-iMac.mshome.net>
2024-07-08 18:22:21 -0500

    fixed about me section and photo, added hero image

--- app/src/components/about/index.jsx
+++ app/src/components/about/index.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './about.css'; 
 import ProfilePic from '../../assets/ProfilePic.jpg';
+import HeroImage from '../../assets/HeroImage.jpg'; // Ensure this path is correct
 
 const About = () => {
   return (
-    <div className="about-me-section">
-      <div className="about-me-content">
-        <h1>About Me</h1>
-        <p>
-          Welcome to my portfolio! Here you can learn more about me and my work.
-        </p>
+    <div>
+      <div className="hero-image-section">
+        <div className="hero-image-container">
+          <img
+            src={HeroImage}
+            alt="DoSomethingImage"
+            className="do-something-image"
+          />
+        </div>
       </div>
-      <div className="about-me-image-container">
-        <img
-          src={ProfilePic}
-          alt="Nikki Vargas Pic"
-          className="about-me-image"
-        />
+      <div className="about-me-section">
+        <div className="about-me-content">
+          <h1>About Me</h1>
+          <p>
+            Welcome to my portfolio! Here you can learn more about me and my work.
+          </p>
+        </div>
+        <div className="about-me-image-container">
+          <img
+            src={ProfilePic}
+            alt="Nikki Vargas Pic"
+            className="about-me-image"
+          />
+        </div>
       </div>
     </div>
   );
